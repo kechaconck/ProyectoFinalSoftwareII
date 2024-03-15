0cd51bd387d686bf92804a55dccad26c65b2f2ce
kechaconck <135286809+kechaconck@users.noreply.github.com>
2024-03-14 22:26:22 -0500

    Create Validar.java

---
+++ Software para la venta electronica de libros/CONTROLADORES/Validar.java
@@ -0,0 +1,71 @@
+package Controladores;
+
+import Modelo.Usuario;
+import Modelo.UsuarioDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet(name = "Validar", urlPatterns = {"/Validar"})
+public class Validar extends HttpServlet {
+
+    Usuario usuario = new Usuario();
+    UsuarioDAO usuarioDAO = new UsuarioDAO();
+       
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Validar</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet Validar at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+      
+        String accion = request.getParameter("accion");
+        if (accion.equalsIgnoreCase("Ingresar")) {
+            int documento = Integer.parseInt(request.getParameter("txtusuario"));
+            String pass = request.getParameter("txtpassword");
+            usuario = usuarioDAO.Validar(documento, pass);
+            System.out.print("dato"+usuario.getNombre());
+            if(usuario.getNombre()!= null){
+                System.out.print("dato1"+usuario.getNombre());
+                request.setAttribute("usuario", usuario);
+                request.getRequestDispatcher("Controlador?menu=Principal").forward(request, response);
+
+            }else{
+                request.getRequestDispatcher("index.jsp").forward(request, response);
+            }
+            
+        }else{
+            request.getRequestDispatcher("index.jsp").forward(request, response);
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+}
